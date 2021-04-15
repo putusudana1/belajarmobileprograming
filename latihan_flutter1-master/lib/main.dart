8c5a4c18b13cf0aadb826b6ec275319e0009aa8b
putusudana1 <putusudana1403@gmail.com>
2021-04-15 20:47:24 +0800

    latihan1

---
+++ latihan_flutter1-master/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Stateless Widget Hello World"),
+        ),
+        body: Center(child: Text("190030302_I Putu Sudana")),
+      ),
+    );
+  }
+}
